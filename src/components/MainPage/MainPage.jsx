092db46cd3e1a7d76b8be9cf85d878f48a194fe9
Danila Serpokrylov <70173349+danyafaye@users.noreply.github.com>
2022-03-03 16:11:12 +0300

    feat: improved styles

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -3,8 +3,25 @@ import './MainPage.css'
 
 const MainPage = () => {
     return (
-        <div>
-            <img src="https://www.amocrm.ru/uploads/2021/04/wCqLD-Frame-2-45.jpg" alt=''/>
+        <div className="app-main-content">
+            <img src="https://www.amocrm.ru/uploads/2021/04/wCqLD-Frame-2-45.jpg" alt='' className="app-logo"/>
+            <div className="app-main-content-text">
+                <h2>
+                    Test task by WelbeX
+                </h2>
+                <p>
+                    Vacancy: React.js Web-developer
+                </p>
+                <p>
+                    Need to develop an todo app with two pages (main page and todo list) in the SPA format using these technologies:
+                </p>
+                <ul>
+                    <li>React</li>
+                    <li>Redux</li>
+                    <li>React Router</li>
+                    <li>Axios</li>
+                </ul>
+            </div>
         </div>
     )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,6 +5,7 @@ import {NavLink} from "react-router-dom";
 const Navbar = () => {
     return (
         <nav className="app-navbar">
+            <img src="https://img.icons8.com/ios/500/vertical-line.png" alt="" className="app-line-right"/>
             <NavLink to="/app-main" className="app-main">Main page</NavLink>
             <NavLink to="/app-todo" className="app-todo">Todo list</NavLink>
         </nav>
