6613f919a23e1db7119ed2b7f6a3b204247f9238
Danila Serpokrylov <70173349+danyafaye@users.noreply.github.com>
2022-03-03 03:57:20 +0300

    feat: added styles to SPA

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,8 +5,8 @@ import {NavLink} from "react-router-dom";
 const Navbar = () => {
     return (
         <nav className="app-navbar">
-            <NavLink to="/app-main" className="app-main" activeClassName="app-active">Main page</NavLink>
-            <NavLink to="/app-todo" className="app-todo" activeClassName="app-active">Todo list</NavLink>
+            <NavLink to="/app-main" className="app-main">Main page</NavLink>
+            <NavLink to="/app-todo" className="app-todo">Todo list</NavLink>
         </nav>
     )
 }
